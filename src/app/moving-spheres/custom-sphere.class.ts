0f0f6e1c2cf7942a9d317c673421dce922c16dc2
fonzane <timschaffert@yahoo.de>
2022-05-12 19:19:47 +0200

    further playing around (with movement)

--- src/app/moving-spheres/custom-sphere.class.ts
+++ src/app/moving-spheres/custom-sphere.class.ts
@@ -3,4 +3,6 @@ import { Mesh } from "three";
 export interface CustomSphere extends Mesh {
     moveUpward?: boolean;
     increaseYSpeed?: boolean;
+    xMoveSpeed?: number;
+    yMoveSpeed?: number;
 }
--- src/app/moving-spheres/moving-spheres.component.ts
+++ src/app/moving-spheres/moving-spheres.component.ts
@@ -1,6 +1,6 @@
 import { NgtRenderState, NgtStore } from '@angular-three/core';
 import { Component, OnInit } from '@angular/core';
-import { firstValueFrom } from 'rxjs';
+import { firstValueFrom, timer } from 'rxjs';
 import { Mesh, Vector3 } from 'three';
 import { CustomSphere } from './custom-sphere.class';
 
@@ -11,8 +11,10 @@ import { CustomSphere } from './custom-sphere.class';
 })
 export class MovingSpheresComponent {
 
-  xMoveSpeed = 0.01;
-  yMoveSpeed = 0.01;
+  // xMoveSpeed = 0.01;
+  // yMoveSpeed = 0.01;
+
+  sphereCloned = false;
 
   spheres: CustomSphere[] = [];
 
@@ -21,43 +23,59 @@ export class MovingSpheresComponent {
   onSphereReady(sphere: CustomSphere) {
     sphere.moveUpward = true;
     sphere.increaseYSpeed = true;
+    sphere.xMoveSpeed = 0.01;
+    sphere.yMoveSpeed = 0.01;
+
     this.spheres.push(sphere);
   }
 
   onMeshBeforeRender($event: {state: NgtRenderState, object: Mesh}) {
-    let sphere = $event.object;
+    let sphere = $event.object as CustomSphere;
 
-    this.spheres.forEach(sphere => {
+    this.spheres.forEach(s => {
       // moving the sphere
-      sphere.position.x += 0.01;
-      if (sphere.moveUpward) sphere.position.y += this.yMoveSpeed;
-      if (!sphere.moveUpward) sphere.position.y -= this.yMoveSpeed;
+      s.position.x += 0.01;
+      if (s.moveUpward) s.position.y += s.yMoveSpeed!;
+      if (!s.moveUpward) s.position.y -= s.yMoveSpeed!;
       
-      if (sphere.position.y >= 2.5) {
-        sphere.moveUpward = false;
-        sphere.increaseYSpeed = true;
-        this.yMoveSpeed = 0.01;
+      if (s.position.y >= 2.5) {
+        s.moveUpward = false;
+        s.increaseYSpeed = true;
+        s.yMoveSpeed = 0.01;
       };
-      if (sphere.position.y <= -2.5) {
-        sphere.moveUpward = true;
-        sphere.increaseYSpeed = true;
-        this.yMoveSpeed = 0.01;
+      if (s.position.y <= -2.5) {
+        s.moveUpward = true;
+        s.increaseYSpeed = true;
+        s.yMoveSpeed = 0.01;
       };
   
       // setting spheres movementspeed
-      if (sphere.increaseYSpeed) this.yMoveSpeed += 0.0005;
-      if (!sphere.increaseYSpeed) this.yMoveSpeed -= 0.005;
+      if (s.increaseYSpeed) s.yMoveSpeed! += 0.0005;
+      if (!s.increaseYSpeed) s.yMoveSpeed! -= 0.005;
     })
+
+    if (sphere.position.y >= 2.5) this.cloneSphere(sphere);
+    if (sphere.position.y <= -2.5) this.cloneSphere(sphere);
 
   }
 
   async cloneSphere(sphere: CustomSphere) {
-    let clonedSphere = sphere.clone();
-    clonedSphere.position.set(-3, 0, 0);
+    if (!this.sphereCloned) {
+      let clonedSphere = sphere.clone();
 
-    let scene = await firstValueFrom(this.store.scene$);
-    scene.add(clonedSphere);
-    this.spheres.push(clonedSphere);
+      clonedSphere.moveUpward = true;
+      clonedSphere.increaseYSpeed = true;
+      sphere.xMoveSpeed = 0.01;
+      sphere.yMoveSpeed = 0.01;
+      clonedSphere.position.set(-3, 0, 0);
+  
+      let scene = await firstValueFrom(this.store.scene$);
+      scene.add(clonedSphere);
+      this.spheres.push(clonedSphere);
+    }
+
+    this.sphereCloned = true;
+    timer(500).subscribe(() => this.sphereCloned = false);
   }
 
 }
