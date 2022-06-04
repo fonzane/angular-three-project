90d2e254ccb82fb5673793e635cd11496e85a03d
fonzane <timschaffert@yahoo.de>
2022-06-04 19:42:09 +0200

    added some fun

--- src/app/geometry-spawner/geometry-spawner.component.ts
+++ src/app/geometry-spawner/geometry-spawner.component.ts
@@ -1,8 +1,7 @@
 import { NgtRenderState, NgtStore } from '@angular-three/core';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { interval, timer } from 'rxjs';
-import { Euler, Group, Mesh, MeshBasicMaterial, MeshStandardMaterial, RingGeometry } from 'three';
-import { GLTF, GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { Euler, Group, MathUtils, Mesh, MeshBasicMaterial, MeshStandardMaterial, RingGeometry } from 'three';
 import { GeometryService } from '../geometry.service';
 
 @Component({
@@ -11,13 +10,16 @@ import { GeometryService } from '../geometry.service';
   styleUrls: ['./geometry-spawner.component.scss']
 })
 export class GeometrySpawnerComponent implements OnInit {
-  @ViewChild('mesh')mesh?: Mesh
+  @ViewChild('mesh')mesh?: Mesh;
+  @Input('blog-link')blogLink?: HTMLAnchorElement;
 
   rings: Mesh<RingGeometry, MeshStandardMaterial>[] = [];
-  loader: GLTFLoader = new GLTFLoader();
-  xwing?: Group;
   xRotationSpeed = 0.02;
   zMoveSpeed = 0.01;
+  blogLinkMovX = 1;
+  blogLinkMovY = 1;
+
+  trollEmojis = ['🤣', '😂', '😜', '🤑', '😳', '👻', '🤡', '🖕'];
 
   constructor(
     private store: NgtStore,
@@ -42,8 +44,27 @@ export class GeometrySpawnerComponent implements OnInit {
       clone.rotationSpeed = Math.random() > 0.5 ? Math.random() * this.xRotationSpeed +0.005: -(Math.random()*this.xRotationSpeed+0.005);
       scene.add(clone);
       this.rings.push(clone);
-    })
-    // interval(1500).subscribe(() => this.xRotationSpeed = Math.random() > 0.5 ? -Math.random()*0.025 : Math.random()*0.025);
+    });
+    if(this.blogLink) {
+      this.blogLink.style.right = "0px";
+      this.blogLink.style.top = "0px";
+      timer(1000).subscribe(() => {
+        interval(4000).subscribe(() => {
+          this.blogLink!.style.right = "0px";
+          this.blogLink!.style.top = "0px";
+          this.blogLink!.innerHTML = "Blog";
+          this.blogLink!.style.boxShadow = "0 0 2px 2px white";
+        })
+      })
+      interval(4000).subscribe(() => {
+        this.blogLink!.style.top = MathUtils.randInt(0, window.innerHeight) + "px";
+        this.blogLink!.style.right = MathUtils.randInt(0, window.innerWidth) + "px";
+        this.blogLink!.innerHTML = this.trollEmojis[MathUtils.randInt(0, this.trollEmojis.length-1)];
+        let boxShadow = `0 0 ${Math.random()*5+2}px ${Math.random()*5+2}px rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`;
+        this.blogLink!.style.boxShadow = boxShadow;
+        console.log(this.blogLink?.style.boxShadow, boxShadow);
+      });
+    };
   }
 
   async onMeshBeforeRender($event: {state: NgtRenderState, object: Mesh}) {
@@ -63,11 +84,9 @@ export class GeometrySpawnerComponent implements OnInit {
         this.rings.shift();
       }
     })
-
-    if(this.xwing) this.xwing.position.z -= 0.5;
-
+    if (this.blogLink) {
+    }
   }
-
 }
 
 interface CustomRing extends Mesh<RingGeometry, MeshStandardMaterial> {
